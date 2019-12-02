807aa833601a45ae4294616e40d128efbb26799d
Puyan Wei <puyan.wei@mountainwarehouse.com>
2019-12-02 13:31:52 +0000

    Updated App.js to ES6

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import {
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	}
 });
+
+const App = () => {
+	return (
+		<View style={styles.container}>
+			<Text>Hello</Text>
+		</View>
+	);
+}
+
+export default App;
